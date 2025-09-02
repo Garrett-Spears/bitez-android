c3422c672c8d3283961da4dcd989a86343fbeb98
gns12 <gns12@MacBookPro.lan>
2025-09-01 22:07:59 -0400

    Added fragment library as build dependency and refactored MainActivity to use FragmentContainerView.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.fragment.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
