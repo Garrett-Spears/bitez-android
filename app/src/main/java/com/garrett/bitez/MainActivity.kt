db789b9370d6a93ea02358781813c2b746880758
gns12 <gns12@MacBookPro.lan>
2025-09-01 22:15:07 -0400

    Added AppCompatActivity as dependency and changed MainActivity to use AppCompatActivity instead of ComposeActivity.

--- app/src/main/java/com/garrett/bitez/MainActivity.kt
+++ app/src/main/java/com/garrett/bitez/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.garrett.bitez
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.garrett.bitez.ui.theme.BitezTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
