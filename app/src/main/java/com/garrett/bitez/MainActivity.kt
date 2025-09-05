e65d2a5fb5f7cc5447f314d75d727e34f8e92e10
gns12 <gns12@Garretts-MacBook-Pro.local>
2025-09-05 17:49:41 -0400

    Added listener to detect item select on each bottom nav view tab.

--- app/src/main/java/com/garrett/bitez/MainActivity.kt
+++ app/src/main/java/com/garrett/bitez/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.garrett.bitez
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.fragment.app.Fragment
 import com.garrett.bitez.ui.theme.BitezTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,11 +19,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Only add default fragment if this is first creation
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, Fragment())
-                .commit()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_container, Fragment())
+//                .commit()
+//        }
+
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+
+        // Set up click listeners for bottom navigation tabs
+        bottomNavView.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                // Figure out which tab to switch to
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        println("Clicked on Home")
+                    }
+                    R.id.navigation_search -> {
+                        println("Clicked on search")
+                    }
+                    R.id.navigation_profile -> {
+                        println("Clicked on profile")
+                    }
+                    else -> {
+                        // Should never get here
+                        println("Clicked on bottom nav view but couldn't find tab.")
+                        return false
+                    }
+                }
+                return true
+            }
+        })
     }
 }
 
