2e26fe7139a6a70557a06e8c7f5de2dd8c687914
gns12 <gns12@Garretts-MacBook-Pro.local>
2025-09-05 19:32:58 -0400

    Connected bottom navigation view to open three different fragments.

--- app/src/main/java/com/garrett/bitez/MainActivity.kt
+++ app/src/main/java/com/garrett/bitez/MainActivity.kt
@@ -1,70 +1,70 @@
 package com.garrett.bitez
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.garrett.bitez.ui.theme.BitezTheme
+
+import dagger.hilt.android.AndroidEntryPoint
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
-import dagger.hilt.android.AndroidEntryPoint
+
+import com.garrett.bitez.ui.home.HomeFragment
+import com.garrett.bitez.ui.explore.ExploreFragment
+import com.garrett.bitez.ui.profile.ProfileFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Only add default fragment if this is first creation
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragment_container, Fragment())
-//                .commit()
-//        }
+        // Only switch to tab if this is the first time the activity is created
+        if (savedInstanceState == null) {
+            switchTab(R.id.navigation_home)
+        }
 
         val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
 
         // Set up click listeners for bottom navigation tabs
         bottomNavView.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                // Figure out which tab to switch to
-                when (item.itemId) {
-                    R.id.navigation_home -> {
-                        println("Clicked on Home")
-                    }
-                    R.id.navigation_search -> {
-                        println("Clicked on search")
-                    }
-                    R.id.navigation_profile -> {
-                        println("Clicked on profile")
-                    }
-                    else -> {
-                        // Should never get here
-                        println("Clicked on bottom nav view but couldn't find tab.")
-                        return false
-                    }
-                }
-                return true
+                return switchTab(item.itemId)
             }
         })
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BitezTheme {
-        Greeting("Android")
+    // Helper function to switch between fragment tabs
+    private fun switchTab(itemId: Int): Boolean {
+        // Figure out which tab to switch to
+        when (itemId) {
+            R.id.navigation_home -> {
+                Log.d(TAG, "Switching to home tab")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment())
+                    .commit()
+            }
+            R.id.navigation_explore -> {
+                Log.d(TAG, "Switching to explore tab")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ExploreFragment())
+                    .commit()
+            }
+            R.id.navigation_profile -> {
+                Log.d(TAG, "Switching to profile tab")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment())
+                    .commit()
+            }
+            else -> {
+                // Should never get here
+                Log.w(TAG, "Clicked on bottom nav view but couldn't find tab.")
+                return false
+            }
+        }
+        return true
     }
 }
